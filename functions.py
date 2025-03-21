c8d7fca1d8b34434c7f45b689f1f57d90f5709f7
Moi4664 <119817370+Moi4664@users.noreply.github.com>
2025-03-21 17:22:45 +0100

    Completion+determinization+affichage
    normalement c'est pas mal

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ from os import walk
 from prettytable import PrettyTable 
 
 alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
+figures = [str(i) for i in range(10)]
 
 
 ### **Readind the infos of the FA**
@@ -58,13 +58,16 @@ def print_fa_info(file):
 ### **Displaying the FA informations in a table**
 def create_fa_table(fa_info):
     nb_letters, nb_states, nb_entry, pos_entry, nb_terminal, pos_terminal, nb_transitions, list_transitions = fa_info
-    col_list = ["", "States"] + alphabet[:nb_letters]
+
+
+    states = []
+    letters = []
+    #on regarde après le premier element (car le premier element peux être une lettre aussi) quand est ce qu'on rencontre qqch qui n'est pas un chiffre
+    states = States(fa_info)
+    letters = Letters(fa_info)
+
+    col_list = ["", "States"] + letters[:]
     myTable = PrettyTable(col_list)
-
-
-    states = [str(state) for state in range(nb_states)]
-    if "i" in pos_entry:
-        states.insert(0, "i")
 
     for state in states:
         row = []
@@ -79,13 +82,21 @@ def create_fa_table(fa_info):
 
         row.append(state)
 
-        for letter in range(nb_letters):
-            temp = [transition[2:] for transition in list_transitions if transition[0] == str(state) and transition[1] == alphabet[letter]]
+        for letter in letters:
+            temp=[]
+            for transition in list_transitions:
+                placeOfLetter=0
+                while transition[placeOfLetter] not in letters:
+                    placeOfLetter+=1
+                if transition[placeOfLetter] == letter and transition[:placeOfLetter] == state:
+                    temp.append("".join([transition[placeOfLetter+1:]]))
             row.append(", ".join(temp) if temp else "-")
-
+        
         myTable.add_row(row)
-
+    
+    
     return myTable
+
 
 def print_fa_table(table):
     print(table)
@@ -168,22 +179,33 @@ def standardization(fa_info):
 def completion(fa_info):
     if not is_complete(fa_info):
         nb_letters, nb_states, nb_entry, pos_entry, nb_terminal, pos_terminal, nb_transitions, list_transitions = fa_info
-        nb_states +=1 #rajout de l'état P
-        #rajout de ses transitions
+        cplt_nb_states = 1+nb_states #rajout de l'état P
+        #creation d'une copie des transitions
+        cplt_list_transitions = []
+        cplt_nb_transitions = nb_transitions
+        for i in list_transitions:
+            cplt_list_transitions.append(i)
+        #rajout de ses transitions (ex : PaP, PbP...)
         for i in range(nb_letters):
             transition = f"P{alphabet[i]}P"
-            list_transitions.append(transition)
-            nb_transitions +=1
+            cplt_list_transitions.append(transition)
+            cplt_nb_transitions +=1
         #rajout des transitions incomplete
-        for state in range(nb_states-1): #on doit soutraire 1 car sinon cela rajoute un état indésirable 
-            for letter in range(nb_letters):
-                if not any(transition[0] == str(state) and transition[1] == alphabet[letter] for transition in list_transitions):
-                    list_transitions.append(f"{state}{alphabet[letter]}P")
-                    nb_transitions+=1
-        for transition in list_transitions:
+        states = States(fa_info)
+        letters = Letters(fa_info)
+        
+        for state in states:
+            placeOfLetter = len(state)
+            for letter in letters:
+                if not any(transition[:placeOfLetter] == str(state) and transition[placeOfLetter] == letter for transition in list_transitions): 
+                    cplt_list_transitions.append(f"{state}{letter}P")
+                    cplt_nb_transitions+=1
+
+        for transition in cplt_list_transitions:
             print(f"  - {transition}")
         print("-" * 40)
-        print_fa_table(create_fa_table(fa_info))
+        cplt_fa_info = (nb_letters,cplt_nb_states,nb_entry,pos_entry,nb_terminal,pos_terminal,cplt_nb_transitions,cplt_list_transitions)
+        return tuple(cplt_fa_info)
     else :
         print("your table is already completed ^^")
 
@@ -192,23 +214,21 @@ def determinization_and_completion(fa_info):
         print("your automata is already deterministic and complete")
     else:
         if is_deterministic(fa_info):
-            print("your automata is now completed")
-            completion(fa_info)
+            fa = completion(fa)
         if is_complete(fa_info):
-            determiniaztion(fa_info)
+            fa = determiniaztion(fa_info)
         else:
-            completion(fa_info)
-            determiniaztion(fa_info)
+            fa = determiniaztion(fa_info)
+            fa = completion(fa)
+    return fa
+            
 
 def determiniaztion(fa_info):
     if not is_deterministic(fa_info):
         nb_letters, nb_states, nb_entry, pos_entry, nb_terminal, pos_terminal, nb_transitions, list_transitions = fa_info
-        new_nb_states = 0
-        new_nb_entry = 1
         new_pos_entry = []
         new_nb_terminal = 0
         new_pos_terminal = []
-        new_nb_transitions = 0
         new_list_transitions = []
         if (nb_entry > 1):
             entry=""
@@ -217,22 +237,73 @@ def determiniaztion(fa_info):
             new_pos_entry.append(entry)
         else:
             new_pos_entry = pos_entry
-        new_nb_states +=1
-
-        transition_int = []
-        for state in new_pos_entry[0]:
+ 
+        new_states = [new_pos_entry[0]]
+        n= 0
+        i=0
+        while (len(new_states) != n):      
+            trans = []
+            letters = []
+            word = ""
             for transition in list_transitions:
-                if state == transition[0]:
-                    for letter in range(nb_letters):
-                        if transition[1] == alphabet[letter] and transition[2:] not in a:
-                            a.append(transition[2:])
-                        
+                placeOfLetter = 1
+                while transition[placeOfLetter] in figures:
+                    placeOfLetter+=1
+                if str(transition[:placeOfLetter]) in str(new_states[n]):
+                    if str(transition[placeOfLetter:]) not in trans:
+                        trans.append(transition[placeOfLetter:])
+                        if transition[placeOfLetter] not in letters:
+                            letters.append(str(transition[placeOfLetter]))
+            
+            for letter in letters:
+                output = False #set the element as not an output by default
+                new_list_transitions.append(str(new_states[n])+letter)
+                for subtrans in trans:
+                    if letter in subtrans:
+                        new_list_transitions[i]+=subtrans[1:]
+                word = new_list_transitions[i][len(str(new_states[n]))+1:]
+                word = Sortstring(word)
+
+                tmp = []
+
+                for k in range(len(new_list_transitions[i])):
+                    if k < len(str(new_states[n]))+1:
+                        tmp.append(new_list_transitions[i][k])
+                    else:
+                        tmp.append(Sortstring(new_list_transitions[i][len(str(new_states[n]))+1:])[k-len(str(new_states[n]))-1])
                     
-        if a != [] and b != [] :
-            new_list_transitions.append(f"{new_pos_entry[0]}a{trans_a}")
-            new_list_transitions.append(f"{new_pos_entry[0]}b{trans_b}")
-        for i in new_list_transitions:
-            print(i)
+                new_list_transitions[i] = "".join(tmp)
+                
+                
+                if any(int(k) in pos_terminal for k in word):
+                    output = True
+                if str(word) not in new_states:
+                    new_states.append(word)
+                    #adding the element to the output list
+                    if output:
+                        new_pos_terminal.append(word)
+                        new_nb_terminal+=1
+                i+=1
+            n+=1
+        
+        determinized_fa_info = [] #We create a new variable to not change the original one
+        
+        #copying data
+        for k in fa_info:
+            determinized_fa_info.append(k)
+
+        #change the element to have a deteminized automata
+        determinized_fa_info[1] = n #redefine the number of states
+        determinized_fa_info[2] = 1 #redefine the number of entry (for determinization it's always equal to 1)
+        determinized_fa_info[3] = new_pos_entry # redefine which element is the entry
+        determinized_fa_info[4] = new_nb_terminal # redefine the number of terminal/output
+        determinized_fa_info[5] = new_pos_terminal # redefine which elements are the output
+        determinized_fa_info[-2] = len(new_list_transitions) #redefine the number of transition
+        determinized_fa_info[-1] = new_list_transitions #redefine the transitions
+
+        return tuple(determinized_fa_info)
+    else:
+        return fa_info
 
 def int_to_roman(num):
     roman_numerals = {
@@ -344,6 +415,57 @@ def test_recognize_word(fa_info):
         else:
             print(f"No, '{word}' is not recognizable by the automata")
 
+
+
+### **Useful function**
+
+def States(fa_info):
+    nb_letters, nb_states, nb_entry, pos_entry, nb_terminal, pos_terminal, nb_transitions, list_transitions = fa_info
+
+    states = []
+
+    #on regarde après le premier element (car le premier element peux être une lettre aussi) quand est ce qu'on rencontre qqch qui n'est pas un chiffre
+    for state in list_transitions:
+        placeOfLetter=1
+        while state[placeOfLetter] in figures:
+            placeOfLetter+=1
+        if state[:placeOfLetter] not in states :
+            states.append(state[:placeOfLetter])
+        if state[placeOfLetter+1:] not in states:
+            states.append(state[placeOfLetter+1:])
+    return states
+
+def Letters(fa_info):
+    nb_letters, nb_states, nb_entry, pos_entry, nb_terminal, pos_terminal, nb_transitions, list_transitions = fa_info
+
+    letters = []
+
+    for state in list_transitions:
+        placeOfLetter=1
+        while state[placeOfLetter] in figures:
+            placeOfLetter+=1
+        if state[placeOfLetter] not in letters :
+            letters.append(state[placeOfLetter])
+
+    return letters
+
+def Transition(fa_info):
+    nb_letters, nb_states, nb_entry, pos_entry, nb_terminal, pos_terminal, nb_transitions, list_transitions = fa_info
+
+    Transition = []
+
+    for state in list_transitions:
+        placeOfLetter=1
+        while state[placeOfLetter] in figures:
+            placeOfLetter+=1
+        if state[:placeOfLetter] not in Transition :
+            Transition.append(state[placeOfLetter])
+            
+    return Transition
+
+#permet de ranger en ordre croissant une chaine de caractère de chiffre (ex: "6954" deviens "4569")
+def Sortstring(word):
+    return "".join(list(map(str,sorted(list(map(int,list(word)))))))
 
 ### **Usage**
 file = "./automatas/39.txt"
